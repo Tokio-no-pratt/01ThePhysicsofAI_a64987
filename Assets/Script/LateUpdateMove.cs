08fda4a7e54fd2b972da38a6034e71d3fdd2f6eb
Tokio <suelennascimento164@gmail.com>
2026-09-22 18:17:51 +0100

    testando a parte da explicação

---
+++ Assets/Script/LateUpdateMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LateUpdateMove : MonoBehaviour
+{
+    public float force = 10f;
+    public Transform target;
+    public Transform CameraTransform;
+    public float SpeedTreeWindAsset = 5f;
+    public Rigidbody rb;
+
+
+  
+    void FixedUpdate()
+    {
+        rb.AddForce (Vector3 . forward * force);
+    }
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * SpeedTreeWindAsset * Time.deltaTime);
+    }
+    void LateUpdate()
+    {
+        CameraTransform.LookAt(target);
+    }
+ 
+
+}
