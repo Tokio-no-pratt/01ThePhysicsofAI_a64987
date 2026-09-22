958396bfc7854dbc1ef9f153e12cacd73c1b8a44
Tokio <suelennascimento164@gmail.com>
2026-09-22 18:52:11 +0100

    tem algumas falhas

--- Assets/Script/LateUpdateMove.cs
+++ Assets/Script/LateUpdateMove.cs
@@ -4,26 +4,10 @@ using UnityEngine;
 
 public class LateUpdateMove : MonoBehaviour
 {
-    public float force = 10f;
-    public Transform target;
-    public Transform CameraTransform;
-    public float SpeedTreeWindAsset = 5f;
-    public Rigidbody rb;
-
-
-  
-    void FixedUpdate()
-    {
-        rb.AddForce (Vector3 . forward * force);
-    }
-    private void Update()
-    {
-        transform.Translate(Vector3.forward * SpeedTreeWindAsset * Time.deltaTime);
-    }
     void LateUpdate()
     {
-        CameraTransform.LookAt(target);
+        this.transform.Translate(0, 0, 0.01f);
     }
- 
+
 
 }
--- Assets/Script/UpdateMove.cs
+++ Assets/Script/UpdateMove.cs
@@ -7,6 +7,5 @@ public class UpdateMove : MonoBehaviour
 
     void Update()
     {
-
-    }
-}
+        this.transform.Translate(0, 0, 0, Time.deltaTime);
+        S
